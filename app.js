11e7ad717fd1fd30db087ceec5a23d0f646b2d97
Seth Earby <fenderfan23@gmail.com>
2015-07-15 21:49:50 -0500

    Created create, login, and logout functionality.

--- app.js
+++ app.js
@@ -28,40 +28,15 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-var passport = require('passport')
-    , LocalStrategy = require('passport-local').Strategy;
-
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-        User.findOne({ username: username }, function (err, user) {
-            if (err) { return done(err); }
-            if (!user) {
-                return done(null, false, { message: 'Incorrect username.' });
-            }
-            if (!user.validPassword(password)) {
-                return done(null, false, { message: 'Incorrect password.' });
-            }
-            return done(null, user);
-        });
-    }
-));
-
-passport.serializeUser(function(user, done)
-{
-    done(null, user.username);
-});
-
-passport.deserializeUser(function(username, done){
-    done(null, {username: username});
-});
-
-
 // Using the flash middleware provided by connect-flash to store messages in session
 // and displaying in templates
 var flash = require('connect-flash');
 app.use(flash());
 
 // Initialize Passport
+var initPassport = require('./passport/init');
+initPassport(passport);
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+
+var isAuthenticated = function (req, res, next) {
+  // if user is authenticated in the session, call the next() to call the next request handler
+  // Passport adds this method to request object. A middleware is allowed to add properties to
+  // request and response objects
+  if (req.isAuthenticated())
+    return next();
+  // if the user is not authenticated then redirect him to the login page
+  res.redirect('/');
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -26,5 +36,15 @@ router.post('/signup', passport.authenticate('signup', {
   failureFlash : true
 }));
 
+/* GET Home Page */
+router.get('/home', isAuthenticated, function(req, res){
+  res.render('home', { user: req.user });
+});
+
+/* Handle Logout */
+router.get('/signout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
 
 module.exports = router;
